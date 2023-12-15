a4623c7c45fa35b6b907b7ddbb4fc05807cadac5
jeanbarcenas24 <152881024+jeanbarcenas24@users.noreply.github.com>
2023-12-15 14:54:04 +0800

    Create word.js

---
+++ word.js
@@ -0,0 +1,21 @@
+
+        word : "Cat",
+        hint : "fur-bearing animal"
+    },
+    {
+        word : "Moon",
+        hint : "Earth s only natural satellite"
+    },
+    {
+        word : "Leaves",
+        hint : "The tree has a branch filled with green _____."
+    },
+    {
+        word : "Muscles",
+        hint : "part of the body helps you move"
+    },
+    {
+        word : "Nostrils",
+        hint : "The two holes of the nose"
+    },
+]
